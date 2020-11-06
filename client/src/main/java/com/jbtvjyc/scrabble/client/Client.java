23c853685b17cacaa9551083dba32fd7aa8b3a06
Johanna Delachambre <delachambre.johanna@gmail.com>
2020-11-06 18:13:09 +0100

    Correction Classe Client #6

--- client/src/main/java/com/jbtvjyc/scrabble/client/Client.java
+++ client/src/main/java/com/jbtvjyc/scrabble/client/Client.java
@@ -3,15 +3,13 @@ package com.jbtvjyc.scrabble.client;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+@Component
+@Scope("singleton")
 public class Client {
-    @Component
-    @Scope("singleton")
-    public class Client {
 
-        public MotPositionne jouer(EtatDuJeu etatDuJeu) {
-            System.out.println("Joueur > je joue mot au milieu");
-            return new MotPositionne("mot", 7, 7) ;
-        }
+    public MotPositionne jouer(EtatDuJeu etatDuJeu) {
+        System.out.println("Joueur > je joue mot au milieu");
+        return new MotPositionne("mot", 7, 7);
     }
 
 }
