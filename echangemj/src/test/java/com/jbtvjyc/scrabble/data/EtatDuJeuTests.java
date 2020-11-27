130885c4c600f0bbcc888cac72dc02779ba82208
cguibourg-58 <42938472+cguibourg-58@users.noreply.github.com>
2020-11-27 23:15:03 +0100

    correct error

--- echangemj/src/test/java/com/jbtvjyc/scrabble/data/EtatDuJeuTests.java
+++ echangemj/src/test/java/com/jbtvjyc/scrabble/data/EtatDuJeuTests.java
@@ -1,5 +1,10 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import com.jbtvjyc.scrabble.data.EtatDuJeu;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class EtatDuJeuTest {
 
--- echangemj/src/test/java/com/jbtvjyc/scrabble/data/IdentificationTests.java
+++ echangemj/src/test/java/com/jbtvjyc/scrabble/data/IdentificationTests.java
@@ -1,5 +1,12 @@
+package com.jbtvjyc.scrabble.data;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import com.jbtvjyc.scrabble.data.Identification;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class IdentificationTest {
 
