e0a2e31fb470425616914b25fe487059eac0f5c7
Slime-Senpai <mrthetawtute@hotmail.fr>
2020-12-01 10:41:24 +0100

    Revert "Revert "Testunitaires""
    This reverts commit bef63b67c7fa2c8fcff1077d5d8391de6515f5bf.

---
+++ echangemj/src/test/java/com/jbtvjyc/scrabble/data/MotPositionneTests.java
@@ -0,0 +1,52 @@
+package com.jbtvjyc.scrabble.data;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MotPositionneTests {
+
+    @Test
+    void testSetGetMot() {
+        MotPositionne motPositionne= new MotPositionne();
+        motPositionne.setMot("cheval");
+        Assertions.assertEquals( "cheval", motPositionne.getMot());
+    }
+
+    @Test
+    void testSetGetAbscisse() {
+        MotPositionne motPositionne= new MotPositionne();
+        motPositionne.setAbscisse(5);
+        Assertions.assertEquals( 5, motPositionne.getAbscisse());
+    }
+
+    @Test
+    void testSetGetOrdonnee() {
+        MotPositionne motPositionne= new MotPositionne();
+        motPositionne.setOrdonnee(4);
+        Assertions.assertEquals( 4, motPositionne.getOrdonnee());
+    }
+
+    @Test
+    void testSetGetHorizontal() {
+        MotPositionne motPositionne= new MotPositionne();
+        motPositionne.setHorizontal(false);
+        Assertions.assertEquals( false, motPositionne.getHorizontal());
+    }
+
+    @Test
+    void testToString() {
+        MotPositionne motPositionne= new MotPositionne();
+        String dir = "horizontal";
+
+        if (!motPositionne.getHorizontal()) {
+            dir = "vertical";
+        }
+        Assertions.assertEquals( "(" + motPositionne.getMot() + "," + motPositionne.getAbscisse() + "," + motPositionne.getOrdonnee() + "," + dir + ")", motPositionne.toString());
+    }
+}
