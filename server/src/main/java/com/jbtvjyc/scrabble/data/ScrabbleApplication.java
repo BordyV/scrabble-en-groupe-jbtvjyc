fd77101e8f4cec772ed5c3a406ab86ba99191aba
Slime-Senpai <mrthetawtute@hotmail.fr>
2020-12-15 17:43:47 +0100

    Ajout test d'intégration avec Docker.

--- server/src/main/java/com/jbtvjyc/scrabble/data/ScrabbleApplication.java
+++ server/src/main/java/com/jbtvjyc/scrabble/data/ScrabbleApplication.java
@@ -1,13 +1,25 @@
 package com.jbtvjyc.scrabble.data;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ScrabbleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScrabbleApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner aGame(@Autowired Moteur moteur) {
+		return args -> {
+			if (args.length > 0) {
+				moteur.setExitOnFinish(true);
+			}
+		};
 	}
 
 }
--- server/src/test/java/com/jbtvjyc/scrabble/data/MoteurWebControlleurITCase.java
+++ server/src/test/java/com/jbtvjyc/scrabble/data/MoteurWebControlleurITCase.java
@@ -5,16 +5,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.util.ReflectionTestUtils;
-import com.jbtvjyc.scrabble.data.EtatDuJeu;
-import com.jbtvjyc.scrabble.data.Identification;
-import com.jbtvjyc.scrabble.data.Moteur;
-import com.jbtvjyc.scrabble.data.MoteurWebControlleur;
 
 import java.util.concurrent.TimeUnit;
 
@@ -25,54 +19,60 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 
-@WebMvcTest(MoteurWebControlleur.class)
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
 public class MoteurWebControlleurITCase {
 
     // data
-    Identification id;
+    Identification id ;
     EtatDuJeu etat;
 
-    @MockBean
-    Moteur moteur;
-
-    @MockBean
+    @SpyBean
     MoteurWebControlleur webControlleur;
 
+    @Autowired
+    Moteur moteur;
+
+    Moteur mSpy;
+
     @BeforeEach
-    void setUp() throws Exception {
+    void setUp()throws Exception {
         id = new Identification("Joueur pour intégration", "http://127.0.0.1:8081/");
         etat = new EtatDuJeu();
 
-        // ReflectionTestUtils.setField(webControlleur, "moteur", moteur);
+        mSpy = Mockito.spy(moteur);
+        ReflectionTestUtils.setField(webControlleur, "moteur", mSpy);
     }
 
     @Test
     void demanderAuJoueurDeJouerTest() {
+
         // pour que le controlleur ait un joueur...
         // l'appel extérieur qui lance tout
         webControlleur.getValue(id);
 
         // some code are in a thread...
         try {
-            TimeUnit.MILLISECONDS.sleep(2000);
+            TimeUnit.MILLISECONDS.sleep(3000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
 
-        verify(moteur, times(1)).lancerPartie();
-        verify(webControlleur, times(2)).demanderAuJoueurDeJouer(any());
+        verify(mSpy, times(1)).lancerPartie();
+
+        verify(webControlleur, times(40)).demanderAuJoueurDeJouer(any());
         verify(webControlleur, times(1)).envoyerFin();
 
+        // TODO Il faudrait réussir à faire placer un certain nombre de mots sur le plateau pour le tester
+        // EtatDuJeu plateau = mSpy.getEtatDuJeu();
+        // assertEquals(2, plateau.getListeDeMots().size());
 
-        EtatDuJeu plateau = moteur.getEtatDuJeu();
-        assertEquals(2, plateau.getListeDeMots().size());
-
-        verify(moteur, times(3)).getEtatDuJeu();
+        verify(mSpy, times(40)).getEtatDuJeu();
         // error to verify it is a good spy // verify(mSpy, times(16)).aMethod();
 
         // normalement, à la fin le client est éteint
-        assertThrows(org.springframework.web.client.ResourceAccessException.class, () -> moteur.run());
+        assertThrows(org.springframework.web.client.ResourceAccessException.class, () -> mSpy.run());
 
         // etc.
 
@@ -91,26 +91,26 @@ public class MoteurWebControlleurITCase {
 
         // some code are in a thread...
         try {
-            TimeUnit.MILLISECONDS.sleep(2000);
+            TimeUnit.MILLISECONDS.sleep(3000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
 
         // 2 fois lancerPartie, mais une seule avec effet... comme c'est un sout, on ne peut pas vérfier la 2e fois si ce n'est que le reste ne change pas
-        verify(moteur, times(2)).lancerPartie();
-        verify(webControlleur, times(2)).demanderAuJoueurDeJouer(any());
+        verify(mSpy, times(2)).lancerPartie();
+        verify(webControlleur, times(40)).demanderAuJoueurDeJouer(any());
         verify(webControlleur, times(1)).envoyerFin();
 
+        // TODO Pareil que demanderAuJoueurDeJoueurTest
+        // EtatDuJeu plateau = mSpy.getEtatDuJeu();
+        // assertEquals(2, plateau.getListeDeMots().size());
 
-        EtatDuJeu plateau = moteur.getEtatDuJeu();
-        assertEquals(2, plateau.getListeDeMots().size());
-
-        verify(moteur, times(3)).getEtatDuJeu();
+        verify(mSpy, times(40)).getEtatDuJeu();
         // error to verify it is a good spy // verify(mSpy, times(16)).aMethod();
 
         // normalement, à la fin le client est éteint
-        assertThrows(org.springframework.web.client.ResourceAccessException.class, () -> moteur.run());
+        assertThrows(org.springframework.web.client.ResourceAccessException.class, () -> mSpy.run());
 
         // etc.
 
